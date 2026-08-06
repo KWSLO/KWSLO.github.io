80e5e1ff9ea5bb84967d3581c375cfa90599b8ef
KWSLO <swwaaaae@outlook.com>
2026-08-06 23:53:02 +0800

    Site updated: 2026-08-06 23:53:02

---
+++ js/music.js
@@ -0,0 +1,24 @@
+(function () {
+    if (window.musicInit) return
+    window.musicInit = true
+
+    function initMusic() {
+        const box = document.getElementById('music-player')
+        if (!box) return
+
+        const meting = document.createElement('meting-js')
+
+        meting.setAttribute('server', 'netease')
+        meting.setAttribute('type', 'playlist')
+        meting.setAttribute('id', '12595519825')
+        meting.setAttribute('fixed', 'true')
+
+        box.appendChild(meting)
+    }
+
+    if (customElements.get('meting-js')) {
+        initMusic()
+    } else {
+        window.addEventListener('load', initMusic)
+    }
+})()
