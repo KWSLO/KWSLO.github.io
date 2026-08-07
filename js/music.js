1f182aaec817443fe60090d25ae604dbfc91884e
KWSLO <swwaaaae@outlook.com>
2026-08-08 01:41:33 +0800

    Site updated: 2026-08-08 01:41:33

--- js/music.js
+++ js/music.js
@@ -1,25 +1,39 @@
 (function () {
-    if (window.musicInit) return
-    window.musicInit = true
+if (window.musicInit) return
+window.musicInit = true
 
-    function initMusic() {
-        const box = document.getElementById('music-player')
-        if (!box) return
+function hideLrc(){
+    const timer=setInterval(()=>{
+        const lrcButton=document.querySelector('.aplayer-icon-lrc')
+        if(lrcButton){
+            lrcButton.click()
+            clearInterval(timer)
+        }
+    },200)
+}
 
-        const meting = document.createElement('meting-js')
 
-        meting.setAttribute('server', 'netease')
-        meting.setAttribute('type', 'playlist')
-        meting.setAttribute('id', '12595519825')
-        meting.setAttribute('fixed', 'true')
-        meting.setAttribute('lrc', '0')
+function initMusic() {
+    const box = document.getElementById('music-player')
+    if (!box) return
 
-        box.appendChild(meting)
-    }
+    const meting = document.createElement('meting-js')
 
-    if (customElements.get('meting-js')) {
-        initMusic()
-    } else {
-        window.addEventListener('load', initMusic)
-    }
+    meting.setAttribute('server', 'netease')
+    meting.setAttribute('type', 'playlist')
+    meting.setAttribute('id', '12595519825')
+    meting.setAttribute('fixed', 'true')
+
+    box.appendChild(meting)
+
+    hideLrc()
+}
+
+
+if (customElements.get('meting-js')) {
+    initMusic()
+} else {
+    window.addEventListener('load', initMusic)
+}
+
 })()
